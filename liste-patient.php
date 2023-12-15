e4bcdfe54d003babb4baf8fcc8018111bda18b03
AKNL <aknlsame@gmail.com>
2023-12-15 16:40:16 +0100

    seconde commit

--- liste-patient.php
+++ liste-patient.php
@@ -5,9 +5,8 @@ include_once("./partials/header.php");
 
 $request = $db->query('SELECT * FROM patients');
 
-$patients=$request -> fetchAll(); {
+$patients = $request->fetchAll()
 
- 
 ?>
 
 
@@ -20,46 +19,47 @@ $patients=$request -> fetchAll(); {
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
- 
 
 
 
- <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">Lastname</th>
-      <th scope="col">Firstname</th>
-      <th scope="col">Birthdate</th>
-      <th scope="col">Phone</th>
-      <th scope="col">Email</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-        <?php
-  foreach($patients as $row);
 
-} ?>
-      <th scope="row">1</th>
-      <td><?php echo $row["lastname"] ?></td>
-      <td><?php echo $row["firstname"] ?></td>
-      <td><?php echo $row["birthdate"] ?></td>
-      <td><?php echo $row["phone"] ?></td>
-      <td><?php echo $row["mail"] ?></td>
-    </tr>
-  
-     
-  </tbody>
-</table>
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Lastname</th>
+                <th scope="col">Firstname</th>
+                <th scope="col">Actions</th>
+
+            </tr>
+        </thead>
+        <tbody>
+
+            <?php foreach ($patients as $patient) { ?>
+                <tr>
+                    <th scope="row"><?php echo $patient["id"] ?></th>
+                    <td><?php echo $patient["lastname"] ?></td>
+                    <td><?php echo $patient["firstname"] ?></td>
+                    <td>bouton en voir plus</td>
+                </tr>
+
+            <?php } ?>
+
+
+
+        </tbody>
+    </table>
 
 
 
 </body>
+
 </html>
